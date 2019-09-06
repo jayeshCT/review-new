7bc1e34d39cb4e4f18682b0263cc4e174c951d76
jayeshCT <jayeshkhandagle123@gmail.com>
2019-09-07 04:00:56 +0530

    Initial

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
  
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/review/dist'));
-
+a
 app.get('/*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + './review/dist/index.html'))
+  res.sendFile(path.join(__dirname, 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
