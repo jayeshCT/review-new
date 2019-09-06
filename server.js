abdbc4f30a63d721b41d9db10353cae383c451a3
jayeshCT <jayeshkhandagle123@gmail.com>
2019-09-06 22:33:09 +0530

    Adding changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/review-new'));
+app.use(express.static(__dirname + '/dist/'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/review-new/index.html'));
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
