d1708232fa4fb43216cae2d14b4526ded184eba9
jayeshCT <jayeshkhandagle123@gmail.com>
2019-09-07 04:16:49 +0530

    Initial

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const express = require('express');
 const path = require('path');   
 
 const app = express();   
+// Serve static files....
+app.use(express.static(__dirname + '/dist/floating-refuge-53579'));
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
-});  
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/floating-refuge-53579/index.html'));
+});
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
