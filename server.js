45a43c8df17578aeebdcf710080ace64f5c4f7b3
jayeshCT <jayeshkhandagle123@gmail.com>
2019-09-07 04:10:28 +0530

    Initial

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-//Install express server
+//Install express server    
 const express = require('express');
-const path = require('path');
- 
-const app = express();
- 
-// Replace the '/dist/<to_your_project_name>'
-a
-app.get('/*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname, 'index.html'))
-});
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
 
-console.log("Console listining !!");
+const path = require('path');   
+
+const app = express();   
+
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
