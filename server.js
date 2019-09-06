21662ce6a75e018a08e1c657635474fc01ed68de
jayeshCT <jayeshkhandagle123@gmail.com>
2019-09-07 04:23:14 +0530

    Initial

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 
 const app = express();   
 // Serve static files....
-app.use(express.static(__dirname + '/dist/review'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/review/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port    
