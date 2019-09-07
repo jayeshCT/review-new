5952db66d309b6c3d828ed842694d626a0f418e3
jayeshCT <jayeshkhandagle123@gmail.com>
2019-09-07 12:15:47 +0530

    commit

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const forceSSL = function () {
         next();
     }
 }
-app.use(express.static(__dirname + '/dist/review'));
+app.use(express.static(__dirname + '/dist/floating-refuge-53579'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/review/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/floating-refuge-53579/index.html'));
 });
 // Start the app by listening on the default
 // Heroku port
