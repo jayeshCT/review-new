53d89a027297038e2b88c04b309f4b101807ff34
jayeshCT <jayeshkhandagle123@gmail.com>
2019-09-07 12:08:51 +0530

    commit

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-//Install express server    
 const express = require('express');
+const app = express();
+const path = require('path');
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+app.use(express.static(__dirname + '/dist/review'));
 
-const path = require('path');   
-
-const app = express();   
-// Serve static files....
-app.use(express.static(__dirname + '/dist'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/review/index.html'));
 });
-
-// Start the app by listening on the default Heroku port    
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
